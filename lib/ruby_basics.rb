76c0c67345dbb94b9c44de6c3f862af64317ff21
Eduardo <edubrodriguez@gmail.com>
2015-12-06 22:18:16 +0000

    Done with Ruby Basics Review

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
 def division(num1, num2)
- 
+ num1 = 24
+ num2 = 4
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  "#{value}"
 end
+assign_variable("Eduardo")
 
-def argue
-  
+def argue (phrase)
+  "#{phrase}"
 end
+argue ("This is the phrase")
 
-def greeting
-
+def greeting (greet, name)
+  "#{greet}{name}"
 end
+greeting("Hello", "Eduardo")
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  puts "Return the phrase expert"
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party (ingredient = "cheese")
+  "#{ingredient}"
 end
+pizza_party()
+pizza_party("salami")
